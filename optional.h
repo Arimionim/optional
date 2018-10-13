04b968d849d540571e47a2c70e5c536e21c8652a
Arslan <arimionim@yandex.ru>
2018-10-13 15:01:24 +0300

    fixed

--- optional.h
+++ optional.h
@@ -1,73 +1,69 @@
-//
-// Created by Arslan on 13.10.2018.
-//
+#ifndef OPTIONAL_H
+#define OPTIONAL_H
 
-#ifndef OPTIONAL_OPTIONAL_H
-#define OPTIONAL_OPTIONAL_H
+#include <iostream>
+#include <type_traits>
+#include <string>
 
-#include <algorithm>
+template<class T>
+class optional {
+    typename std::aligned_storage<sizeof(T), alignof(T)>::type data[1];
+    bool full = 0;
 
-template<typename T>
-struct optional{
-    optional() : empty(true){}
+public:
+    optional() = default;
 
     optional(T const & val){
-        data = val;
-        empty = false;
+        clear();
+        full = 1;
+        new(reinterpret_cast<T *>(data)) T(val);
     }
 
-    optional(optional const & other){
-        auto tmp = other;
-        swap(tmp);
-    }
-
-    optional &operator=(optional const & other){
-        empty = other.empty;
-        if (!empty){
-            data = other.data;
+    optional(optional const &other): full(other.full) {
+        if (full) {
+            new(reinterpret_cast<T*>(data)) T(*reinterpret_cast<const T *>(other.data));
         }
+    }
+
+    optional &operator=(optional const &other) {
+        optional tmp = other;
+        swap(tmp);
         return *this;
     }
 
-    void clear(){
-        empty = true;
+    ~optional() {
+        clear();
     }
 
-    explicit operator bool() const{
-        return !empty;
+    explicit operator bool() const {
+        return full;
     }
 
-    T &operator*(){
-        if (!empty) {
-            return data;
-        }
-        else{
-            throw std::logic_error("optional is empty");
+    void clear() {
+        if (full) {
+            reinterpret_cast<T *>(data)->~T();
+            full = 0;
         }
     }
 
-    T *operator->(){
-        if (!empty) {
-            return &data;
-        }
-        else{
-            throw std::logic_error("optional is empty");
-        }
-    }
-
-    void swap(optional<T> & other){
-        std::swap(empty, other.empty);
+    void swap(optional &other){
         std::swap(data, other.data);
+        std::swap(full, other.full);
     }
 
-private:
-    bool empty;
-    T data;
+
+    T &operator*() {
+        return *reinterpret_cast<T*>(data);
+    }
+
+    T *operator->() {
+        return (reinterpret_cast<T*>(data));
+    }
 };
 
 template <typename T>
-void swap(optional<T> &a, optional<T> &b){
+void swap(optional<T> a, optional<T> b){
     a.swap(b);
 }
 
-#endif //OPTIONAL_OPTIONAL_H
+#endif //OPTIONAL_H
